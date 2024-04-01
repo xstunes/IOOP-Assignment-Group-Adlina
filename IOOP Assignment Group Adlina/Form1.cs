eecba25fdb0b8e594e36a5678a7fcbb88fec6425
HAIDER TAYYEBA <TP077884@mail.apu.edu.my>
2024-04-01 08:58:24 +0800

    added the button

--- IOOP Assignment Group Adlina/Form1.cs
+++ IOOP Assignment Group Adlina/Form1.cs
@@ -16,5 +16,10 @@ namespace IOOP_Assignment_Group_Adlina
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
