d4f1b798cc0581d5d801407f50c03cb66a014b0a
Adlina <nikowimvu@gmail.com>
2024-04-01 09:26:21 +0800

    Removed test tools.

--- IOOP Assignment Group Adlina/Form1.Designer.cs
+++ IOOP Assignment Group Adlina/Form1.Designer.cs
@@ -29,58 +29,24 @@
         private void InitializeComponent()
         {
             this.hello = new System.Windows.Forms.Label();
-            this.test = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // hello
             // 
             this.hello.AutoSize = true;
             this.hello.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hello.Location = new System.Drawing.Point(322, 196);
+            this.hello.Location = new System.Drawing.Point(286, 157);
             this.hello.Name = "hello";
-            this.hello.Size = new System.Drawing.Size(276, 82);
+            this.hello.Size = new System.Drawing.Size(231, 69);
             this.hello.TabIndex = 0;
             this.hello.Text = "HELLO";
             // 
-            // test
-            // 
-            this.test.AutoSize = true;
-            this.test.Location = new System.Drawing.Point(423, 304);
-            this.test.Name = "test";
-            this.test.Size = new System.Drawing.Size(36, 20);
-            this.test.TabIndex = 1;
-            this.test.Text = "test";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(316, 74);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(264, 26);
-            this.textBox1.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(144, 234);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.test);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.hello);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -91,9 +57,6 @@
         #endregion
 
         private System.Windows.Forms.Label hello;
-        private System.Windows.Forms.Label test;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
