2bb9d771d147de1c3bb7a824435dbc0cd3ed4700
LIM ZHI HENG <TP077563@mail.apu.edu.my>
2024-04-01 09:36:58 +0800

    added button, test

--- IOOP Assignment Group Adlina/Form1.Designer.cs
+++ IOOP Assignment Group Adlina/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.hello = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // hello
@@ -41,11 +42,21 @@
             this.hello.TabIndex = 0;
             this.hello.Text = "HELLO";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 19);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.hello);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Label hello;
+        private System.Windows.Forms.Button button1;
     }
 }
 
