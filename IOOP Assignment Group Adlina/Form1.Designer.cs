4957616a72137997d16154535a2917b46c00566e
CHIANG XIANG ZHI <TP077553@mail.apu.edu.my>
2024-04-03 11:15:30 +0800

    test

--- IOOP Assignment Group Adlina/Form1.Designer.cs
+++ IOOP Assignment Group Adlina/Form1.Designer.cs
@@ -29,35 +29,25 @@
         private void InitializeComponent()
         {
             this.hello = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // hello
             // 
             this.hello.AutoSize = true;
             this.hello.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hello.Location = new System.Drawing.Point(286, 157);
+            this.hello.Location = new System.Drawing.Point(322, 196);
             this.hello.Name = "hello";
-            this.hello.Size = new System.Drawing.Size(231, 69);
+            this.hello.Size = new System.Drawing.Size(276, 82);
             this.hello.TabIndex = 0;
             this.hello.Text = "HELLO";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(102, 55);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(65, 19);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(900, 562);
             this.Controls.Add(this.hello);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -68,7 +58,6 @@
         #endregion
 
         private System.Windows.Forms.Label hello;
-        private System.Windows.Forms.Button button1;
     }
 }
 
