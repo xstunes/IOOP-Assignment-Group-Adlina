df597c89788ce683d45ec28de2b37442945bf5e2
CHIANG XIANG ZHI <TP077553@mail.apu.edu.my>
2024-04-03 11:19:10 +0800

    test2

--- IOOP Assignment Group Adlina/Form1.Designer.cs
+++ IOOP Assignment Group Adlina/Form1.Designer.cs
@@ -30,34 +30,47 @@
         {
             this.hello = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // hello
             // 
             this.hello.AutoSize = true;
             this.hello.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hello.Location = new System.Drawing.Point(286, 157);
+            this.hello.Location = new System.Drawing.Point(322, 196);
             this.hello.Name = "hello";
-            this.hello.Size = new System.Drawing.Size(231, 69);
+            this.hello.Size = new System.Drawing.Size(276, 82);
             this.hello.TabIndex = 0;
             this.hello.Text = "HELLO";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(528, 25);
+            this.button1.Location = new System.Drawing.Point(594, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(219, 97);
+            this.button1.Size = new System.Drawing.Size(246, 121);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(145, 89);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 2;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.hello);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -69,6 +82,7 @@
 
         private System.Windows.Forms.Label hello;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
