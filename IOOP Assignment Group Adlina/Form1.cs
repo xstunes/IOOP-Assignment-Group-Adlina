a5dc0521d65d293db896e3f5762dfe2678f76fff
Faee <nikowimvu@gmail.com>
2024-05-13 21:01:51 +0800

    small changes -nothing notable.

--- IOOP Assignment Group Adlina/Form1.cs
+++ IOOP Assignment Group Adlina/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient; //importing the system data sql client. 
+using System.Configuration;
+using System.Runtime.Remoting.Contexts;
 
 namespace IOOP_Assignment_Group_Adlina
 {
@@ -23,10 +25,21 @@ namespace IOOP_Assignment_Group_Adlina
 
         }
 
+
+
+
+        public string Login(string un)
+        {
+            string status = null;
+            SqlConnection con = new SqlConnection(@"Data Source=USER;Initial Catalog=admin;Integrated Security=True;"); //Database connection
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand(SELECT count(*) from userData where Username=LblLoginUserEmail or Email=LblLoginUserEmail and Password=LblLoginPass)
+        }
+
         private void BtnLogin_Click(object sender, EventArgs e) //When clicking login
         {
-            //1. Creating a SQL connection
-            
+           
 
         }
     }
