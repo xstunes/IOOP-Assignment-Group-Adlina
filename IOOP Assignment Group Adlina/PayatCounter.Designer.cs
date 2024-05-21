d8c2c2af08b93ed42b968b02e4f4504d4d7d83f9
HAIDER TAYYEBA <TP077884@mail.apu.edu.my>
2024-05-21 09:48:30 +0800

    Added pay at counter form

--- IOOP Assignment Group Adlina/PayatCounter.Designer.cs
+++ IOOP Assignment Group Adlina/PayatCounter.Designer.cs
@@ -29,17 +29,37 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(244, 37);
+            this.label1.Location = new System.Drawing.Point(335, 145);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(249, 32);
+            this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Counter Payment";
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(126, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(545, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please Proceed To Pay At The Counter";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(621, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK!";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // PayatCounter
             // 
@@ -47,6 +67,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Bisque;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "PayatCounter";
             this.Text = "PayatCounter";
@@ -58,5 +80,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
