0a320ea371404799f1116d5bea52cff6b9c8f180
LIM ZHI HENG <TP077563@mail.apu.edu.my>
2024-06-06 09:01:35 +0800

    done view orders &update profile

---
+++ IOOP Assignment Group Adlina/Chef.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace IOOP_Assignment_Group_Adlina
+{
+    internal class Chef
+    {
+        private string username;
+        private string email;
+        private string password;
+        static SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MainDB"].ToString());
+
+        public Chef(string name)
+        {
+            username = name;
+            email = null;
+            password = null;
+        }
+
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        
+        public string updateP(string em, string p)
+        {
+            string status;
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("update userData set Email=@e, Password=@p where Username=@nm", con);
+            cmd.Parameters.AddWithValue("@e", em);
+            cmd.Parameters.AddWithValue("@p", p);
+            cmd.Parameters.AddWithValue("@nm", username);
+
+            int i = cmd .ExecuteNonQuery();
+            if (i != 0)
+                status = "Update Successfully";
+            else
+                status = "Unable to update";
+            con.Close();
+            return status;
+        }
+
+        public static void viewP(Chef o1)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand ("select * from userData where Username = @a", con);
+            cmd.Parameters.AddWithValue("@a", o1.username);
+
+            SqlDataReader rd = cmd.ExecuteReader();
+            while (rd.Read()) 
+            {
+                o1.email = rd.GetString(2);
+                o1.password = rd.GetString(3);
+            }
+            con.Close();
+        }
+    }
+}
---
+++ IOOP Assignment Group Adlina/ManageInventory.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace IOOP_Assignment_Group_Adlina
+{
+    partial class ManageInventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listInventory = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(249, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Manage Inventory";
+            // 
+            // listInventory
+            // 
+            this.listInventory.FormattingEnabled = true;
+            this.listInventory.ItemHeight = 16;
+            this.listInventory.Location = new System.Drawing.Point(140, 99);
+            this.listInventory.Name = "listInventory";
+            this.listInventory.Size = new System.Drawing.Size(517, 276);
+            this.listInventory.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(500, 381);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(581, 381);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(419, 380);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // ManageInventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listInventory);
+            this.Controls.Add(this.label1);
+            this.Name = "ManageInventory";
+            this.Text = "ManageInventory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listInventory;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
